6490531c6502d80ae144a1f2bef3046bf95a535a
Brant Oliphant <brant@oliphantllc.com>
2016-10-13 12:37:13 -0800

    moved fxml to correct path for maven javafx plugin

--- PDFTextExtract/src/main/java/panowow/pdfscaner/PdfTextchecker.java
+++ PDFTextExtract/src/main/java/panowow/pdfscaner/PdfTextchecker.java
@@ -56,7 +56,7 @@ public class PdfTextchecker extends Application {
     @Override
     public void start(final Stage stage) throws IOException {
     
-    	Parent root = FXMLLoader.load(getClass().getResource("/fxml/Pdftext.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("Pdftext.fxml"));
 		Scene scene = new Scene(root);
 
 		stage.setTitle("Pick PDF directory");
